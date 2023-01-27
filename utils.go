1c44d5d9d9d538a9a47f4f55ddee5a30901e70e4
Roman Weber <caniwebdev@gmail.com>
2023-01-27 17:34:25 +0300

    up

--- utils.go
+++ utils.go
@@ -1,10 +1,13 @@
 package utils
 
+import "fmt"
+
 func Contains(a []string, x string) bool {
 	for _, n := range a {
 		if n == x {
 			return true
 		}
 	}
+	fmt.Println("Not found")
 	return false
 }
