9ee82effd9831fd79130c28d16a47624bb321989
Roman Weber <caniwebdev@gmail.com>
2023-01-27 17:43:14 +0300

    Add new function ContainsInt

--- utils.go
+++ utils.go
@@ -11,3 +11,13 @@ func Contains(a []string, x string) bool {
 	fmt.Println("Not found")
 	return false
 }
+
+func ContainsInt(a []int, x int) bool {
+	for _, n := range a {
+		if n == x {
+			return true
+		}
+	}
+	fmt.Println("Not found")
+	return false
+}
